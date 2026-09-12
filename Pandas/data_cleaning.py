ea78ac3a29ea89787c8ca1af9ea7bf0b29188f0c
Om Garg <omgarg3000@gmail.com>
2026-09-12 21:28:34 +0530

    pandas basics completed

--- Pandas/data_cleaning.py
+++ Pandas/data_cleaning.py
@@ -114,3 +114,259 @@ df["City"] = df["City"].replace({
 
 # map() is used to transform each value in a Series
 # according to a mapping.
+data={
+    "City": ["Pune","Mumbai","Delhi","Pune","Mumbai"]
+}
+df=pd.DataFrame(data)
+print(df,"\n")
+city_codes = {
+    "Pune": "PN",
+    "Mumbai": "MU",
+    "Delhi": "DL"
+}
+
+df["City_Code"] = df["City"].map(city_codes)
+print(df,"\n")
+
+# apply() is used to apply a function to each value in a Series.
+data = {
+    "Name": ["Om", "Rahul", "Ankit", "Priya"],
+    "Age": [22, None, 21, 24],
+    "City": ["Pune", "Mumbai", None, "Pune"]
+}
+
+def age_increace(age):
+    return age+1
+
+df=pd.DataFrame(data)
+print(df,"\n")
+df["Age"]=df["Age"].apply(age_increace)
+print(df,"\n")
+
+# Using a lambda function
+# You can also write it directly:
+# df["Salary"] = df["Salary"].apply(lambda x: x * 1.10)
+
+# value_counts() tells you how many times each value
+# appears in a column.
+print(df["City"].value_counts(),"\n")
+
+# groupby() is used when you want to divide data into groups
+# based on a column and then perform calculations on each group.
+data = {
+    "Name": ["Om", "Rahul", "Ankit", "Priya", "Neha", "Aman"],
+    "Department": ["IT", "HR", "IT", "HR", "IT", "HR"],
+    "Salary": [35000, 30000, 40000, 45000, 38000, 32000]
+}
+
+df = pd.DataFrame(data)
+df=df.groupby("Department")["Salary"].mean()
+print(df,"\n")
+# syntax:
+# df.groupby("column_to_group_by")["column_to_calculate"].calculation()
+
+# agg() means aggregate.
+# It allows you to perform multiple calculations at once.
+df = pd.DataFrame(data)
+print(df,"\n")
+df = df.groupby("Department")["Salary"].agg(["max","min"])
+print(df,"\n")
+
+# agg() with different functions for different columns
+# Suppose we have:
+# df=df.groupby("Department").agg({
+#     "Salary": ["mean", "max"],
+#     "Age": "min"
+# })
+
+
+# merge() is used to combine two DataFrames based on a common column.
+employees={
+    "ID":[1,2,3,4],
+    "Name":["Om","Rahul","Ankit","Priya"]
+}
+df_employees=pd.DataFrame(employees)
+salary={
+    "ID":[1,2,3,6],
+    "Salary":[300, 400, 500, 100]
+}
+df_salary=pd.DataFrame(salary)
+
+merge=pd.merge(df_employees,df_salary,on="ID")
+print(merge,"\n")
+# syntax 
+# result = pd.merge(
+#     employees,
+#     salaries,
+#     on="ID", column to join on
+#     how="inner" type of join
+# )
+
+# Inner Join
+# An inner join keeps only the rows where the
+# key exists in both DataFrames.
+
+# Left Join
+# A left join keeps everything from the left DataFrame,
+# and matching data from the right DataFrame.
+
+# Right Join
+# A right join keeps everything from the right DataFrame,
+# and matching data from the left DataFrame.
+
+# Outer Join
+# An outer join keeps everything from both DataFrames.
+
+
+# concat() is used to stick DataFrames together.
+# Unlike merge(), it doesn't primarily match rows using a common key.
+concat=pd.concat([df_salary,df_employees], ignore_index=True)
+# ignore_index is to keep a consitant index for the entire
+# Concated table
+print(concat,"\n")
+concat=pd.concat([df_salary,df_employees], ignore_index=False, axis=1)
+# the ignore index with axis 1 turns the column names to id too
+# soo we keep it false 
+print(concat,"\n")
+
+# join() is another way to combine DataFrames,
+# usually using their index.
+employees = pd.DataFrame({
+    "Name": ["Om", "Rahul", "Ankit"],
+    "Age": [22, 23, 21]
+}, index=[1, 2, 3])
+
+salary = pd.DataFrame({
+    "Salary": [35000, 40000, 30000]
+}, index=[1, 2, 3])
+print(employees,"\n")
+print(salary,"\n")
+result = employees.join(salary)
+print(result,"\n")
+# join by default performs left join
+# You can specify another type:
+# employees.join(salary, how="inner")
+# employees.join(salary, how="right")
+# employees.join(salary, how="outer")
+
+# join()
+# Combine mainly based on index.
+
+
+data = {
+    "Name": ["Om", "Rahul", "Ankit", "Priya"],
+    "JoiningDate": ["2024-01-15", "2024-05-20", "2025-02-10", "2025-08-25"]
+}
+df = pd.DataFrame(data)
+# pd.to_datetime() converts values into Pandas' datetime format.
+print(df["JoiningDate"].dtype,"\n")
+df["JoiningDate"]=pd.to_datetime(df["JoiningDate"])
+print(df["JoiningDate"].dtype,"\n")
+# The column is now treated as actual dates rather than ordinary text.
+# Once Pandas recognizes the values as dates, you can easily:
+# Extract year/month/day
+# Compare dates
+# Filter dates
+# Sort dates
+
+
+# extracting year month and day from this 
+print(df["JoiningDate"].dt.year,"\n")
+print(df["JoiningDate"].dt.month,"\n")
+print(df["JoiningDate"].dt.day,"\n")
+
+# Once the column is datetime, you can filter rows based on dates.
+# For example, get employees who joined after January 1, 2025:
+# df[df["JoiningDate"] > "2025-01-01"]
+
+# Filtering within a date range
+# Suppose you want dates between January 1 and June 30, 2025:
+# df[
+#     (df["JoiningDate"] >= "2025-01-01") &
+#     (df["JoiningDate"] <= "2025-06-30")
+# ]
+
+
+data = {
+    "Name": ["Om", "Rahul", "Ankit", "Priya", "Neha", "Aman"],
+    "Age": [22, 23, 21, 24, 22, 25],
+    "Salary": [35000, 40000, 30000, 45000, 38000, 500000],
+    "City": ["Pune", "Mumbai", "Delhi", "Pune", "Mumbai", "Delhi"]
+}
+
+df = pd.DataFrame(data)
+
+# Correlation measures how two numerical variables are related.
+print(df["Age"].corr(df["Salary"]),"\n")
+# print(df["Age"].corr(df["City"]),"\n")
+# strings can not be compared
+
+# The result is between:
+# -1 and +1
+# Positive correlation
+# If one variable tends to increase when the other increases:
+# Negative correlation
+# If one tends to increase while the other decreases:
+
+# You can also calculate correlations between all numerical columns:
+# df.corr(numeric_only=True)
+# Important
+# Correlation does not mean causation.
+# If two things are correlated, it doesn't automatically mean one
+# causes the other.
+
+
+# Unique values are the different values that appear in a column.
+print(df["City"].unique(),"\n")
+
+# Number of unique values
+print(df["City"].nunique(),"\n")
+
+# value_counts() tells you how many times each value appears.
+print(df["Age"].value_counts(),"\n")
+
+
+# Finding outliers using IQR
+# A common method is the IQR (Interquartile Range).
+q1=df["Salary"].quantile(.25)
+q3=df["Salary"].quantile(.75)
+
+iqr=q3-q1
+
+low=q1-1.5*iqr
+high=q3+1.5*iqr
+
+print(df[(
+    (df["Salary"]<low) |
+    (df["Salary"]>high)
+)],"\n")
+
+# EDA = Exploratory Data Analysis.
+# It means:
+# Exploring a dataset to understand what's 
+# inside it before doing deeper analysis.
+
+# Simple EDA Workflow
+# Dataset
+#    ↓
+# head()
+#    ↓
+# shape
+#    ↓
+# columns / dtypes
+#    ↓
+# missing values
+#    ↓
+# duplicates
+#    ↓
+# describe()
+#    ↓
+# unique values / counts
+#    ↓
+# mean / median
+#    ↓
+# correlation
+#    ↓
+# outliers
+#    ↓
+# Understand the dataset
